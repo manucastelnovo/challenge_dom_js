a3ea4e76983a3a7507312a7b7ffb91564654db31
manucastelnovo <marnutskate@gmail.com>
2023-12-13 22:16:27 -0300

    asd

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let menu = document.getElementById("menu");
-let photo= document.getElementById("photo-profile")
-photo.addEventListener("click", () => {
-  if(menu.className.includes('hidden')){
-    menu.classList.remove('hidden');
-  }else{
-    menu.className += " hidden";
-  }
-});
+let input_name = document.getElementById('name')
+let button = document.getElementById('button')
+let table= document.getElementById('lista_todo')
+button.addEventListener('click',()=>{
+    var nuevoElemento = document.createElement("li");
+    var texto = document.createTextNode("Elemento de lista");
+    nuevoElemento.appendChild(texto);
+    table.appendChild('skdjflksadjf');
+})
--- index2.js
+++
@@ -1,9 +0,0 @@
-let h_del_html = document.getElementById('titulo-principal')
-let hdos_del_html = document.getElementById('titulo-secundario')
-console.log(document)
-
-h_del_html.addEventListener('click',()=>{
-    h_del_html.textContent='cambie'
-})
-
-document.ge
